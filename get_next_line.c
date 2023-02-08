02645aafe3680210c6bd97f27023bd0ca75810e8
Kamelia Laksi <klaksi@c1r8p8.42quebec.com>
2023-02-08 15:58:27 -0500

    calloc done

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,31 @@
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 18:43:50 by kamelialaks       #+#    #+#             */
-/*   Updated: 2023/02/08 14:41:22 by klaksi           ###   ########.fr       */
+/*   Updated: 2023/02/08 15:55:56 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void    *ft_calloc(size_t count, size_t size)
+{
+    void *ptr;
+    int i;
+    
+    ptr = malloc(size * count) * sizeof(char);
+    if(!ptr)
+        return (NULL);
+
+    i = 0;
+    while (ptr[i] < (count * size))
+        ptr[i++] = 0;
+    return ((void *)ptr);
+}
+
 char    *ft_strjoint(char *src, char *dest)
 {
     char    *str;
-    
-    str = calloc()
 }
-
 
 
 char	*get_next_line(int fd)
