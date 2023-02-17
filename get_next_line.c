492de628bfc8135dcc7e814b5c76c82f92efbb65
Kamelia Laksi <klaksi@c1r6p8.42quebec.com>
2023-02-17 18:09:34 -0500

    read_buff

--- get_next_line.c
+++ get_next_line.c
@@ -6,43 +6,60 @@
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 18:43:50 by kamelialaks       #+#    #+#             */
-/*   Updated: 2023/02/10 16:10:34 by klaksi           ###   ########.fr       */
+/*   Updated: 2023/02/17 18:08:38 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		if_newline(char *str)
+void    *ft_calloc(size_t element_count, size_t size)
 {
-	size_t	i;
+    char *tab;
+    int i;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n');
-			return (1);
-	}
-	return (0);
-}
-
-char	*read_buff(char *stash, int fd)
-{
-	char 	*buff;
-	
-	buff = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
-	while (if_newline(stash) == 0)
-	{
-		read(fd, buff, BUFFER_SIZE);
-		
-	}
-}
-
-char	*get_next_line(int fd)
-{
-    char *stash;
-
-    if (!stash)
+    i = 0;
+    tab = malloc(element_count * size);
+    if(!tab)
         return(NULL);
-    stash = read_buff();
+    while(i < (element_count * size))
+    {
+        tab[i] = '\0';
+        i++;
+    }
+    return ((void *)tab);
+}
+
+static char *read_buff(char *stash, int fd)
+{
+    char *buff;
+    int return_value;
+    
+    buff = (char *)ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
+    if (!buff)
+        return (NULL);
+    return_value = 1;
+    while (!ft_strchr(stash, '\n') && return_value != 0)
+    {
+        return_value = read(fd, buff, BUFFER_SIZE);
+        if (return_value == -1)
+        {
+            free(buff);
+            free(stash);
+            return (NULL);
+        }
+        buff[return_value] = '\0';
+        stash = ft_strjoin(stash, buff);
+    }
+    free(buff);
     return (stash);
+}
+
+char    *get_next_line(int fd)
+{
+  static char *stash;
+  char *line;
+  
+  if (fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
+    return (NULL);
+    stash = read_buff(stash, fd);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,82 +6,67 @@
 /*   By: klaksi <klaksi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 18:45:17 by kamelialaks       #+#    #+#             */
-/*   Updated: 2023/02/10 17:51:30 by klaksi           ###   ########.fr       */
+/*   Updated: 2023/02/17 18:09:04 by klaksi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int ft_strlen(char *str)
+static size_t   ft_strlen(char *str)
 {
-    unsigned long i;
-    
+    size_t  i;
+
     i = 0;
     while(str[i])
         i++;
     return (i);
 }
 
-void    *ft_calloc(size_t element_count, size_t size)
-{
-	char *tab;
-	int i;
 
-	i = 0;
-	tab = malloc(element_count * size);
-	if(!tab)
-		return(NULL);
-	while(tab[i])
-	{
-		tab[i] = '\0';
-		i++;
-	}
-	return (NULL);
+char    *ft_strjoin(char *s1, char *s2)
+{
+    char *str;
+    size_t i;
+    int len;
+
+    len = ft_strlen(s1) + ft_strlen(s2);
+    if (!s1 || !s2)
+        return (NULL);
+
+    str = ft_calloc(sizeof(char), (len + 1));
+    i = 0;
+    while (s1[i])
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    
+    i = 0;
+    while (s2[i])
+    {
+        str[ft_strlen(s1) + i] = s2[i];
+        i++;
+    }
+    str[len] = '\0';
+    return (str);
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+char    *ft_strchr(const char *s, int c)
 {
-	char *str;
-	size_t i;
-	int len;
+    int i;
 
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!s1 || !s2)
-		return (NULL);
-
-	str = ft_calloc(sizeof(char), (len + 1));
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	
-	i = 0;
-	while (s2[i])
-	{
-		str[ft_strlen(s1) + i] = s2[i];
-		i++;
-	}
-	str[len] = '\0';
-	return (str);
+    if(!s)
+        return (NULL);
+    i = 0;
+    while (s[i])
+    {
+        if (s[i] == (char)c)
+            return ((char *)s + i);
+        i++;
+    }
+    return (NULL);
 }
 
-char	*ft_strchr(char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == (char)c)
-			return ((char *)s + i);
-	i++;
-	}
-	if (s[i] == (char)c)
-		return ((char *)s + i);
-	return (NULL);
-}
 
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
